3437630a589214f16b8669677ce413ef91adc483
Gian Franco <gianfraback@gmail.com>
2023-09-14 02:01:48 -0500

    feat: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createPinia } from "pinia";
+const pinia = createPinia();
 import "./style.css";
 import router from "./router";
 
 const app = createApp(App);
 app.use(router);
+app.use(pinia);
 app.mount("#app");
