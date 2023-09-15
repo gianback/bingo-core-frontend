b612948043ef831fcbc7a035a443474f402e0374
Gian Franco <gianfraback@gmail.com>
2023-09-15 01:23:52 -0500

    feat: add router to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,8 @@
 <template>
-  <header>header</header>
+  <header class="bg-[#313131] text-white text-center pt-4">
+    <ul>
+      <router-link to="/">Go to Home</router-link>
+      <!-- <router-link to="/about">Go to About</router-link> -->
+    </ul>
+  </header>
 </template>
