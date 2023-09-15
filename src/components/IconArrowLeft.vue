077f1740298bca01661c8f7d46e0ec70311e22ff
Gian Franco <gianfraback@gmail.com>
2023-09-15 01:24:07 -0500

    refactor: animation to icon arrow left

--- src/components/IconArrowLeft.vue
+++ src/components/IconArrowLeft.vue
@@ -11,10 +11,18 @@ defineProps<{ isOpen: boolean }>();
     height="24"
     viewBox="0 -960 960 960"
     width="24"
-    :class="`${isOpen ? 'hidden' : ''}`"
+    :class="`${isOpen ? 'rotateSvg' : ''}`"
   >
     <path
       d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z"
     />
   </svg>
 </template>
+<style scoped>
+svg {
+  transition: transform 0.35s ease-in;
+}
+.rotateSvg {
+  transform: rotate(180deg);
+}
+</style>
