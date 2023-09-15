cfd8a244ce9e332687032a3348a9fa542464dc81
Gian Franco <gianfraback@gmail.com>
2023-09-15 01:24:28 -0500

    feat: add cards to create to cardsstore

--- src/store/cardsStore.ts
+++ src/store/cardsStore.ts
@@ -1,11 +1,19 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { getRandomNumbers } from "../utils/getRandomNumbers";
 
 export const useCardsStore = defineStore("cards", () => {
   const cardList = ref<number[][]>([]);
-  const addCard = (card: number[]) => {
-    cardList.value.push(card);
-  };
+  const historyNumbers = ref<number[]>([]);
 
-  return { cardList, addCard };
+  const addCardsToCreate = (newNumber: number) => {
+    for (let i = 0; i < newNumber; i++) {
+      const newNumberList = getRandomNumbers();
+      cardList.value.push(newNumberList);
+    }
+  };
+  const addHistoryNumber = (newNumber: number) => {
+    historyNumbers.value.push(newNumber);
+  };
+  return { cardList, addCardsToCreate, addHistoryNumber };
 });
