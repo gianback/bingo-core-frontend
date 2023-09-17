98af6d2973712c92413c2abf50036b1b6f6129c0
Gian Franco <gianfraback@gmail.com>
2023-09-16 22:32:21 -0500

    feat: add layout and login,register routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,43 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import Layout from "../Layout.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "main-layout",
+      component: Layout,
+      children: [
+        {
+          path: "",
+          name: "home",
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: HomeView,
+        },
+        {
+          path: "/play-game",
+          name: "play-game",
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import("../views/PlayGameView.vue"),
+        },
+      ],
+    },
+
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
     },
     {
-      path: "/play-game",
-      name: "play-game",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/PlayGameView.vue"),
+      path: "/register",
+      name: "register",
+      component: () => import("../views/RegisterView.vue"),
     },
   ],
 });
