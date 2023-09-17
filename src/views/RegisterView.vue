74040ac5ba6a37c31dd254c6f7895bf1ea715936
Gian Franco <gianfraback@gmail.com>
2023-09-17 15:06:27 -0500

    feat: add submit register

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,3 +1,53 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { registerService } from "../services/register.service";
+import Loader from "../components/Loader.vue";
+import { useRouter } from "vue-router";
+
+const { push } = useRouter();
+const isLoading = ref(false);
+const handleSubmit = async (e: Event) => {
+  e.preventDefault();
+  const form = e.currentTarget as HTMLFormElement;
+  const formData = new FormData(form);
+
+  const values = [...formData.values()];
+  if (values.some((value) => value === "")) return;
+
+  /* password validate */
+  const password = formData.get("password") as string;
+  const password_confirmation = formData.get("password_confirmation") as string;
+
+  if (password !== password_confirmation) return;
+
+  const newUser = {
+    name: formData.get("name") as string,
+    surname: formData.get("surname") as string,
+    lastname: formData.get("lastname") as string,
+    email: formData.get("email") as string,
+    password,
+    password_confirmation,
+  };
+
+  try {
+    isLoading.value = true;
+    const userRegistered = await registerService(newUser);
+
+    if (userRegistered) {
+      form.reset();
+      push("/login");
+    } else {
+      /* TODO: pnpm install vue-sonner */
+      alert("Error registering user");
+    }
+  } catch (error: any) {
+    throw new Error(error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+</script>
+
 <template>
   <section class="bg-gray-50 dark:bg-gray-900">
     <div
@@ -12,7 +62,49 @@
           >
             Create and account
           </h1>
-          <form class="space-y-4 md:space-y-6" action="#">
+          <form class="space-y-4 md:space-y-6" @submit="handleSubmit">
+            <div>
+              <label
+                for="name"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Your name</label
+              >
+              <input
+                type="text"
+                name="name"
+                id="name"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="John"
+              />
+            </div>
+            <div>
+              <label
+                for="surname"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Your surname</label
+              >
+              <input
+                type="text"
+                name="surname"
+                id="surname"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Doe"
+              />
+            </div>
+            <div>
+              <label
+                for="lastname"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Your lastname</label
+              >
+              <input
+                type="text"
+                name="lastname"
+                id="lastname"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Rowland"
+              />
+            </div>
             <div>
               <label
                 for="email"
@@ -43,46 +135,32 @@
             </div>
             <div>
               <label
-                for="confirm-password"
+                for="password_confirmation"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >Confirm password</label
               >
               <input
-                type="confirm-password"
-                name="confirm-password"
-                id="confirm-password"
+                type="password"
+                name="password_confirmation"
+                id="password_confirmation"
                 placeholder="••••••••"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
             </div>
-            <div class="flex items-start">
-              <div class="flex items-center h-5">
-                <input
-                  id="terms"
-                  aria-describedby="terms"
-                  type="checkbox"
-                  class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                />
-              </div>
-              <div class="ml-3 text-sm">
-                <label
-                  for="terms"
-                  class="font-light text-gray-500 dark:text-gray-300"
-                  >I accept the
-                  <a
-                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                    href="#"
-                    >Terms and Conditions</a
-                  ></label
-                >
-              </div>
+            <div class="relative">
+              <button
+                type="submit"
+                class="mx-auto block py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              >
+                Create an account
+              </button>
+              <span
+                v-if="isLoading"
+                class="absolute right-0 top-1/2 -translate-y-1/2"
+              >
+                <Loader />
+              </span>
             </div>
-            <button
-              type="submit"
-              class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            >
-              Create an account
-            </button>
             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
               Already have an account?
               <router-link
