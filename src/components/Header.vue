4292d6cd61e3189c13852f677583a8ee47c97694
Gian Franco <gianfraback@gmail.com>
2023-09-16 22:31:29 -0500

    refactor: bg header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+const { path } = useRoute();
+
+const headerClass = computed(() => {
+  return path === "/" ? "bg-gray-900" : "bg-[#313131]";
+});
+</script>
+
 <template>
-  <header class="bg-[#313131] text-white text-center pt-4">
+  <header :class="[headerClass, 'text-white text-center pt-4']">
     <ul class="flex gap-4 items-center justify-center [&>li bg-red-500]">
       <li>
         <router-link to="/">Home</router-link>
