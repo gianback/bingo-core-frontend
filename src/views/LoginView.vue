9fab48662453c9b589a40713ea713b6391700de7
Gian Franco <gianfraback@gmail.com>
2023-09-19 01:14:39 -0500

    feat: add loader to login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,8 +2,11 @@
 import { useRouter } from "vue-router";
 import { loginService } from "../services/login.service";
 import { userStore, Login } from "../store/userStore";
+import Loader from "../components/Loader.vue";
+import { ref } from "vue";
 
 const store = userStore();
+const isLoading = ref(false);
 const { push } = useRouter();
 
 const handleSubmit = async (e: Event) => {
@@ -15,15 +18,17 @@ const handleSubmit = async (e: Event) => {
   const password = formData.get("password") as string;
 
   try {
+    isLoading.value = true;
     const session = (await loginService(email, password)) as Login;
     store.setUser(session.data);
     store.setIsAuth(true);
-
     if ((session.type = "success")) {
       push("/");
     }
   } catch (error: any) {
     throw new Error(error);
+  } finally {
+    isLoading.value = false;
   }
 };
 </script>
@@ -71,12 +76,20 @@ const handleSubmit = async (e: Event) => {
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
             </div>
-            <button
-              type="submit"
-              class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            >
-              Sign in
-            </button>
+            <div class="relative">
+              <button
+                type="submit"
+                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Sign in
+              </button>
+              <span
+                v-if="isLoading"
+                class="absolute right-0 top-1/2 -translate-y-1/2"
+              >
+                <Loader />
+              </span>
+            </div>
             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
               Don’t have an account yet?
               <router-link
