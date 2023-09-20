a46a06b29c0deac8bd62e0a57d892b543945b5c8
Gian Franco <gianfraback@gmail.com>
2023-09-20 02:17:50 -0500

    feat: create axios interceptor

---
+++ src/interceptors/axios.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  AxiosRequestHeaders,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
+import { getCookie } from "../utils/cookies";
+import { getValidationError } from "../utils/getValidationError";
+import { baseApi } from "../utils/axios";
+export const AxiosInterceptor = () => {
+  const updateHeader = (request: InternalAxiosRequestConfig) => {
+    const token = getCookie("token") as string;
+    const newHeaders = {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    };
+    request.headers = newHeaders as AxiosRequestHeaders;
+
+    return request;
+  };
+
+  baseApi.interceptors.request.use((request) => {
+    return updateHeader(request);
+  });
+
+  baseApi.interceptors.response.use(
+    (response: AxiosResponse) => {
+      return response;
+    },
+    (error) => {
+      console.log("error", getValidationError(error));
+      return Promise.reject(error);
+    }
+  );
+};
