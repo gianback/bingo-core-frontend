5b98ef4239ce424dc536165cce294e1ad26013bf
Gian Franco <gianfraback@gmail.com>
2023-09-20 02:18:39 -0500

    feat: create getValidationError utility

---
+++ src/utils/getValidationError.ts
@@ -0,0 +1,9 @@
+type TypeWithKey<T> = {
+  [key: string]: T;
+};
+export const getValidationError = (errorCode: any) => {
+  const codeMatcher: TypeWithKey<string> = {
+    ERR_NETWORK: "No hay red xc",
+  };
+  return codeMatcher[errorCode];
+};
