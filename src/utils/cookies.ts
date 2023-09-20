e03363372c17f18a9a2caa3e0f189a3d992585e9
Gian Franco <gianfraback@gmail.com>
2023-09-20 02:18:20 -0500

    feat: create cookies utility

---
+++ src/utils/cookies.ts
@@ -0,0 +1,26 @@
+export const setCookie = (name: string, value: any) => {
+  let cookieText = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
+  document.cookie = cookieText;
+};
+
+export const getCookie = (name: string) => {
+  const cookieName = `${encodeURIComponent(name)}=`;
+  const cookie = document.cookie;
+  let value = null;
+
+  const startIndex = cookie.indexOf(cookieName);
+  if (startIndex > -1) {
+    let endIndex = cookie.indexOf(";", startIndex);
+    if (endIndex == -1) {
+      endIndex = cookie.length;
+    }
+    value = decodeURIComponent(
+      cookie.substring(startIndex + name.length, endIndex)
+    );
+  }
+  return value;
+};
+
+export const removeCookie = (name: string) => {
+  setCookie(name, "");
+};
