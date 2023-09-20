d48cd7c2ef658da04b16686bb869a228bb73dc79
Gian Franco <gianfraback@gmail.com>
2023-09-20 02:18:00 -0500

    refactor: add ls to user

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -14,19 +14,21 @@ interface UserData {
   name: string;
   token: string;
 }
-
+type userStore = Pick<UserData, "id" | "name">;
 export const userStore = defineStore("user", () => {
-  const user = ref<UserData>({
-    id: 0,
-    name: "",
-    token: "",
-  });
+  const user = ref<userStore>(
+    JSON.parse(localStorage.getItem("user") as string) || {
+      id: 0,
+      name: "",
+    }
+  );
   let isAuth = ref(
     !!JSON.parse(localStorage.getItem("isAuth") as string) || false
   );
 
-  const setUser = (newUser: UserData) => {
+  const setUser = (newUser: userStore) => {
     user.value = newUser;
+    localStorage.setItem("user", JSON.stringify(newUser));
   };
   const setIsAuth = (status: boolean) => {
     isAuth.value = status;
