f5b3d28c48a5a6c45302109a17e2d443b1a0ac0f
Gian Franco <gianfraback@gmail.com>
2023-10-17 00:10:53 -0500

    feat: add hydratedStore

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,10 +1,17 @@
+import { useEffect, useState } from "react";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 export interface UserData {
-  id: number;
+  email: string;
+  id: string;
   name: string;
   token: string;
 }
+type GetFunctionKeys<T> = {
+  [K in keyof T]: T[K] extends (...args: any[]) => void ? K : never;
+}[keyof T];
+
+type OmittedFunctionKeys<T> = Omit<T, GetFunctionKeys<T>>;
 type userStore = Pick<UserData, "id" | "name">;
 
 type UseUserStore = {
@@ -14,19 +21,38 @@ type UseUserStore = {
   setIsAuth: (isAuth: boolean) => void;
 };
 
+const initialStates = {
+  user: {
+    id: "",
+    name: "",
+  },
+  isAuth: false,
+};
 export const useUserStore = create<UseUserStore>()(
   persist(
     (set) => ({
-      user: {
-        id: 0,
-        name: "",
-      },
-      isAuth: false,
+      user: initialStates.user,
+      isAuth: initialStates.isAuth,
       setUser: (user) => set((_state) => ({ user })),
       setIsAuth: (isAuth) => set((_state) => ({ isAuth })),
     }),
     {
       name: "user-store",
+      getStorage: () => localStorage,
     }
   )
 );
+
+export const useHydratedStore = <
+  T extends keyof OmittedFunctionKeys<UseUserStore>
+>(
+  key: T
+): OmittedFunctionKeys<UseUserStore>[T] => {
+  const [state, setState] = useState(initialStates[key]);
+  const zustandState = useUserStore((persistedState) => persistedState[key]);
+  useEffect(() => {
+    setState(zustandState);
+  }, [zustandState]);
+
+  return state;
+};
