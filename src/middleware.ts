e456eb45fa475c051154087762f7cb8fde431a05
Gian Franco <gianfraback@gmail.com>
2023-10-19 19:49:50 -0500

    fix: token value property fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,25 +3,25 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const { value } = request.cookies.get("token") as RequestCookie;
+  const token = request.cookies.get("token") as RequestCookie;
 
   const resp = await fetch(
     `${process.env.NEXT_PUBLIC_URL_BACKEND}/auth/check-status`,
     {
       method: "GET",
       headers: {
-        Authorization: `Bearer ${value}`,
+        Authorization: `Bearer ${token.value}`,
       },
     }
   );
   const { statusCode } = await resp.json();
-  console.log({ statusCode });
   if (
     request.nextUrl.pathname.startsWith("/play-game") ||
     request.nextUrl.pathname === "/"
   ) {
     if (statusCode === 401) {
       request.cookies.delete("token");
+      request.cookies.set("token", "");
       return NextResponse.redirect(new URL("/login", request.url));
     } else {
       return NextResponse.next();
